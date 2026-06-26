7d2f84e85df686248a10dfb3886f090c8ca68b24
kelei321 <51449002+kelei321@users.noreply.github.com>
2026-06-26 14:21:07 +0800

    build: include uniappx and uts modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,9 +11,22 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
         flatDir { dirs("app/libs") }
+        flatDir { dirs("uniappx/libs") }
+        flatDir { dirs("plugins") }
     }
 }
 
 rootProject.name = "BakingInspectionOffline"
 include(":app")
+include(":uniappx")
+include(":uts-firstui-uvue")
+include(":uts-kux-mlkit-scancode")
+include(":uts-kux-request")
+include(":uts-kux-request-lite")
+include(":uts-kux-request-interceptors")
+include(":uts-uni-installApk")
+include(":uts-unix-ui")
+include(":uts-uts-openSchema")
+include(":uts-z-paging-x")
