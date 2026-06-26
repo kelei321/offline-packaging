2be03cfafa351b809a5bb1dbc0328b024c9f5ffc
kelei321 <51449002+kelei321@users.noreply.github.com>
2026-06-26 14:25:06 +0800

    build: add z paging uts module

---
+++ uts-z-paging-x/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+apply(plugin = "io.dcloud.uts.kotlin")
+
+android {
+    namespace = "uts.sdk.modules.zPagingX"
+    compileSdk = 35
+    buildToolsVersion = "35.0.0"
+
+    defaultConfig { minSdk = 21 }
+}
+
+dependencies {
+    compileOnly(fileTree(mapOf("dir" to "../uniappx/libs", "include" to listOf("*.jar", "*.aar"))))
+    compileOnly(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
+    compileOnly("com.alibaba:fastjson:1.2.83")
+    compileOnly("androidx.core:core-ktx:1.10.1")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+}
