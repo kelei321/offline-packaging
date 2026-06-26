e0b339f2554f4d5602558cd235111001612f6da6
kelei321 <51449002+kelei321@users.noreply.github.com>
2026-06-26 14:21:18 +0800

    build: add uts gradle plugin classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        flatDir { dirs("plugins") }
+    }
+    dependencies {
+        classpath(files("plugins/uts-kotlin-compiler-plugin-0.0.1.jar"))
+        classpath(files("plugins/uts-kotlin-gradle-plugin-0.0.1.jar"))
+    }
+}
+
 plugins {
     id("com.android.application") version "8.7.3" apply false
+    id("com.android.library") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
